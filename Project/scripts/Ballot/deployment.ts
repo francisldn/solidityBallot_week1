dedbdf3b5904cbf14f5815facd878e95bd0cc006
Francis_ldn <fnhh04@gmail.com>
2022-06-24 22:17:48 +0100

    initialise folder

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -4,8 +4,7 @@ import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
 
 // This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
 // Do never expose your keys like this
-const EXPOSED_KEY =
-  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+const EXPOSED_KEY = process.env.PRIVATE_KEY;
 
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
@@ -19,7 +18,7 @@ async function main() {
   const wallet =
     process.env.MNEMONIC && process.env.MNEMONIC.length > 0
       ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+      : new ethers.Wallet(EXPOSED_KEY as string);
   console.log(`Using address ${wallet.address}`);
   const provider = ethers.providers.getDefaultProvider("ropsten");
   const signer = wallet.connect(provider);
@@ -41,6 +40,7 @@ async function main() {
     ballotJson.bytecode,
     signer
   );
+  // deploy ballot contract
   const ballotContract = await ballotFactory.deploy(
     convertStringArrayToBytes32(proposals)
   );
